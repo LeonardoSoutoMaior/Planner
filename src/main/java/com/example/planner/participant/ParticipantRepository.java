9c74910c14ab60fbe8ff410a089003b83dfc35f3
LeonardoSoutoMaior <leo.soutomaior@hotmail.com>
2024-07-10 10:38:09 -0300

    Criação do repository de participants

---
+++ src/main/java/com/example/planner/participant/ParticipantRepository.java
@@ -0,0 +1,8 @@
+package com.example.planner.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+}
